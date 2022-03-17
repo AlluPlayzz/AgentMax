b0d194f0a2eb80343093b9d68e0fa18af0409fcc
AlluPlayzz <90374277+AlluPlayzz@users.noreply.github.com>
2022-03-17 10:20:46 +0000

    updated code. Added a command handler for / and normal commands.

--- index.js
+++ index.js
@@ -1,89 +1,87 @@
-const express = require('express')
-const app = express()
-
+/*================================================*/
+//Express Server
+/*================================================*/
+const express = require('express');
+const app = express();
 app.get('/', (req, res) => {
-  res.send('Running!');
+  res.send('Running!')
 });
-
 app.listen(3000, () => {
-  console.log('success');
+  console.log('Running on https://localhost/3000');
 });
 
-const { Discord, Client, Intents, Collection, MessageEmbed } = require("discord.js");
-const client = new Client({intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MEMBERS"]});
-const { prefix } = require("./config.json")
-const Timeout = new Set();
-const ms = require('ms')
-client.commands = new Collection();
-client.aliases = new Collection();
-["command"].forEach(handler => {
-  require(`./handlers/${handler}`)(client);
+/*================================================*/
+//Setting Up Pkgs
+/*================================================*/
+
+require("dotenv").config();
+const fs = require("fs");
+const { Client, Intents, Collection } = require("discord.js");
+
+const client = new Client({
+	intents: [
+		Intents.FLAGS.GUILDS,
+		Intents.FLAGS.GUILD_MESSAGES
+	]
 });
-const db = require("quick.db");
-const Database = new db
 
-client.on('ready', () => {
- client.user.setActivity('a?help!', { type: 'WATCHING' })
-}) 
+/*================================================*/
+//Initialising Chat Commands
+/*================================================*/
 
- 
+const prefix = "a?"
+client.chatcommands = new Collection();
+const chatcommands = fs.readdirSync("./ChatCommands").filter(file => file.endsWith(".js"));
+for(const file of chatcommands) {
+const chatcommandName = file.split(".")[0]
+const chatcommand = require(`./ChatCommands/${chatcommandName}`)
+client.chatcommands.set(chatcommandName, chatcommand)
+}
 
-
-
-
-
-
-
-
-client.on("messageCreate", async message => {
-
-
-  if (message.author.bot) return;
-  if (!message.guild) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  // If message.member is uncached, cache it.
-  if (!message.member) message.member = await message.guild.fetchMember(message);
-
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  const cmd = args.shift().toLowerCase();
-  if (cmd.length === 0) return;
-  let command = client.commands.get(cmd);
-  if (!command) command = client.commands.get(client.aliases.get(cmd));
-  if (command) {
-    if (command.timeout) {
-      if (Timeout.has(`${message.author.id}${command.name}`)) {
-        let timecommand = ms(command.timeout) / 1000
-        let embed = new MessageEmbed().setColor("RED").setTitle(`**Slow down, you can only use this command every ${timecommand}! seconds**`)
-        return message.reply({embeds:[embed]})
-      } else {
-        command.run(client, message, args);
-        Timeout.add(`${message.author.id}${command.name}`)
-        setTimeout(() => {
-          Timeout.delete(`${message.author.id}${command.name}`)
-        }, command.timeout);
-      }
-    } else {
-      command.run(client, message, args)
-    }
+client.on("messageCreate", message => {
+  if(message.content.startsWith(prefix)) {
+const args = message.content.slice(prefix.length).trim().split(/ + /g)
+const chatcommandName = args.shift()
+const chatcommand = client.chatcommands.get(chatcommandName)
+if(!chatcommand) return
+chatcommand.run(client, message, args)
   }
-});
+})
+
+/*================================================*/
+//Intializing SlashCommands
+/*================================================*/
+
+const commandFiles = fs.readdirSync("./SlashCommands").filter(file => file.endsWith(".js"));
+
+const commands = [];
+
+client.commands = new Collection();
+
+for (const file of commandFiles) {
+	const command = require(`./SlashCommands/${file}`);
+	commands.push(command.data.toJSON());
+	client.commands.set(command.data.name, command);
+}
+
+const eventFiles = fs
+	.readdirSync("./events")
+	.filter(file => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args, commands));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args, commands));
+	}
+}
+
+/*================================================*/
+//Logging In
+/*================================================*/
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-client.login(process.env.token)
+      client.login(process.env.token);
